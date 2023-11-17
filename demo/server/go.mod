ce2d9ae6643ac4e5bd8c2f283590129dcd6723ae
caiodallaqua <cvdallaqua@gmail.com>
2023-11-16 23:02:27 -0300

    write demo data to server

--- demo/server/go.mod
+++ demo/server/go.mod
@@ -8,10 +8,14 @@ require (
 	github.com/mastrasec/vectoria v0.0.0-20230921013557-ddd7a0951653
 	github.com/steinfletcher/apitest v1.5.15
 	github.com/stretchr/testify v1.8.4
+	github.com/xitongsys/parquet-go v1.6.2
+	github.com/xitongsys/parquet-go-source v0.0.0-20230919034749-0b16411e6349
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/apache/arrow/go/arrow v0.0.0-20200730104253-651201b0f516 // indirect
+	github.com/apache/thrift v0.14.2 // indirect
 	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -20,8 +24,8 @@ require (
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
-	github.com/golang/groupcache v0.0.0-20190702054246-869f871628b6 // indirect
-	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.3 // indirect
 	github.com/google/flatbuffers v1.12.1 // indirect
 	github.com/google/uuid v1.3.1 // indirect
@@ -29,14 +33,17 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/pierrec/lz4/v4 v4.1.8 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.49.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	go.opencensus.io v0.22.5 // indirect
-	golang.org/x/net v0.8.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- demo/server/main.go
+++ demo/server/main.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
+	"runtime"
 	"strings"
 	"time"
 
@@ -14,6 +16,8 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/mastrasec/vectoria"
+	"github.com/xitongsys/parquet-go-source/local"
+	"github.com/xitongsys/parquet-go/reader"
 )
 
 const (
@@ -24,11 +28,45 @@ const (
 )
 
 type entrypoint struct {
-	addr string
-	path string
-	app  *fiber.App
-	db   *vectoria.DB
+	addr   string
+	path   string
+	app    *fiber.App
+	db     *vectoria.DB
+	logger *slog.Logger
 }
+
+type addReq struct {
+	IndexName string    `json:"index_name"`
+	ItemID    string    `json:"item_id"`
+	ItemVec   []float64 `json:"item_vec"`
+}
+
+type addRes struct{}
+
+type getReq struct {
+	IndexName string    `json:"index_name"`
+	Query     []float64 `json:"query"`
+	Threshold float64   `json:"threshold"`
+	K         uint32    `json:"k"`
+}
+
+type getRes struct {
+	IDs []string `json:"ids"`
+}
+
+type Captions struct {
+	URL       string    `json:"Url"`
+	Embedding []float64 `json:"Embedding"`
+	// Description string    `json:"Description"`
+	// We may add description later on in order to clean up captions with bad descriptions
+}
+
+type newReq struct {
+	Path     string                `json:"path"`
+	LSHConfs []*vectoria.LSHConfig `json:"lsh_configs"`
+}
+
+type newRes struct{}
 
 func newEntrypoint(addr, path string, shouldLog bool, opts ...vectoria.Options) (*entrypoint, error) {
 	db, err := vectoria.New(path, opts...)
@@ -75,53 +113,37 @@ func (entry *entrypoint) health(ctx *fiber.Ctx) error {
 	return nil
 }
 
-type addReq struct {
-	IndexName string    `json:"index_name"`
-	ItemID    string    `json:"item_id"`
-	ItemVec   []float64 `json:"item_vec"`
-}
-
-type addRes struct{}
-
 func (entry *entrypoint) add(ctx *fiber.Ctx) error {
+	logDebug := entry.logger.With("function", "add")
 	payload := &addReq{}
 
 	if err := ctx.BodyParser(payload); err != nil {
 		log.Println(err)
+		logDebug.Error("unable to parse payload data", "error", err.Error())
 		return ctx.Status(http.StatusBadRequest).SendString("{}")
 	}
 
 	err := entry.db.Add(payload.ItemID, payload.ItemVec, payload.IndexName)
 	if err != nil {
-		log.Println(err)
+		logDebug.Error("unable to add data to database", "error", err.Error())
 		return ctx.Status(http.StatusInternalServerError).SendString("{}")
 	}
 
 	return ctx.Status(http.StatusOK).JSON(&addRes{})
 }
 
-type getReq struct {
-	IndexName string    `json:"index_name"`
-	Query     []float64 `json:"query"`
-	Threshold float64   `json:"threshold"`
-	K         uint32    `json:"k"`
-}
-
-type getRes struct {
-	IDs []string `json:"ids"`
-}
-
 func (entry *entrypoint) get(ctx *fiber.Ctx) error {
+	logDebug := entry.logger.With("function", "get")
 	payload := &getReq{}
 
 	if err := ctx.BodyParser(payload); err != nil {
-		log.Println(err)
+		logDebug.Error("unable to parse payload data", "error", err.Error())
 		return ctx.Status(http.StatusBadRequest).SendString("{}")
 	}
 
 	res, err := entry.db.Get(payload.Query, payload.Threshold, payload.K, payload.IndexName)
 	if err != nil {
-		log.Println(err)
+		logDebug.Error("unable to get data from database", "error", err.Error())
 		return ctx.Status(http.StatusInternalServerError).SendString("{}")
 	}
 
@@ -129,9 +151,12 @@ func (entry *entrypoint) get(ctx *fiber.Ctx) error {
 }
 
 func (entry *entrypoint) new(ctx *fiber.Ctx) error {
+	logDebug := entry.logger.With("function", "new")
+
 	payload := newReq{}
 
 	if err := ctx.BodyParser(payload); err != nil {
+		logDebug.Error("unable to parse payload data", "error", err.Error())
 		return ctx.Status(http.StatusBadRequest).SendString("{}")
 	}
 
@@ -140,15 +165,28 @@ func (entry *entrypoint) new(ctx *fiber.Ctx) error {
 		vectoria.WithIndexLSH(payload.LSHConfs...),
 	)
 	if err != nil {
+		logDebug.Error("unable to create database instance", "error", err.Error())
 		return ctx.Status(http.StatusInternalServerError).SendString("{}")
 	}
 
 	res, err := json.Marshal(newRes{})
 	if err != nil {
+		logDebug.Error("unable to marshal response", "error", err.Error())
 		return ctx.Status(http.StatusInternalServerError).SendString("{}")
 	}
 
 	return ctx.Status(http.StatusOK).Send(res)
+}
+
+func (entry *entrypoint) writeToDB(data []Captions) error {
+	for _, caption := range data {
+		if err := entry.db.Add(caption.URL, caption.Embedding); err != nil {
+			entry.logger.Error("unable to wtite item to database", "function", "writeToDB", "error", err.Error())
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (entry *entrypoint) listen() error {
@@ -157,75 +195,148 @@ func (entry *entrypoint) listen() error {
 	return entry.app.Listen(entry.addr)
 }
 
-func getDataset(url string) (dest string, err error) {
+func (entry *entrypoint) getDataset(url string) (dest string, err error) {
+	logDebug := entry.logger.With("function", "getDataset")
+
 	tokens := strings.Split(url, "/")
 	filename := tokens[len(tokens)-1]
 	destPath := filename
 
 	resp, err := http.Get(url)
 	if err != nil {
-		log.Println("Error making the GET request:", err)
+		logDebug.Error("unable to make get request", "error", err.Error())
 		return "", err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
 		err = fmt.Errorf("bad status code: %d", resp.StatusCode)
-		log.Println(err)
+		logDebug.Error("expected status ok", "error", err.Error())
 		return "", err
 	}
 
 	outFile, err := os.Create(destPath)
 	if err != nil {
-		log.Println("Error creating the destination file:", err)
+		logDebug.Error("unable to create destination file", "error", err.Error())
 		return "", err
 	}
 	defer outFile.Close()
 
 	if _, err = io.Copy(outFile, resp.Body); err != nil {
-		log.Println("Error copying the file:", err)
+		logDebug.Error("unable to copy the file", "error", err.Error())
 		return "", err
 	}
 
 	return destPath, nil
 }
 
-func main() {
-	path := ""
-	addr := "127.0.0.1:8558"
-	remoteDatasetPath := "https://github.com/mastrasec/vectoria/releases/download/demo_dataset/sbu_captions_embeddings.parquet"
+func (entry *entrypoint) readParquet(path string) ([]byte, error) {
+	var err error
+	logDebug := entry.logger.With("function", "readParquet")
 
-	datasetPath, err := getDataset(remoteDatasetPath)
+	fileReader, err := local.NewLocalFileReader(path)
 	if err != nil {
-		log.Println(err)
-		return
+		logDebug.Error("unable to create local file reader", "error", err.Error())
+		return nil, err
+	}
+	defer fileReader.Close()
+
+	parquetReader, err := reader.NewParquetReader(fileReader, nil, 4)
+	if err != nil {
+		logDebug.Error("unable to create parquet reader", "error", err.Error())
+		return nil, err
+	}
+	defer parquetReader.ReadStop()
+
+	rawData, err := parquetReader.ReadByNumber(int(parquetReader.GetNumRows()))
+	if err != nil {
+		logDebug.Error("unable to read raw data", "error", err.Error())
+		return nil, err
 	}
 
-	// TODO: Read parquet.
-	log.Println(datasetPath)
+	data, err := json.Marshal(rawData)
+	if err != nil {
+		logDebug.Error("unable to marshal data to json", "error", err.Error())
+		return nil, err
+	}
 
+	return data, nil
+}
+
+func (entry *entrypoint) getData(remoteDatasetPath string) ([]Captions, error) {
+	logDebug := entry.logger.With("function", "getData")
+
+	entry.logger.Info("downloading dataset")
+	datasetPath, err := entry.getDataset(remoteDatasetPath)
+	if err != nil {
+		logDebug.Error("unable to get dataset", "error", err.Error())
+		return nil, err
+	}
+	entry.logger.Info("finished downloading dataset")
+
+	entry.logger.Info("reading parquet")
+	data, err := entry.readParquet(datasetPath)
+	if err != nil {
+		logDebug.Error("unable to read parquet", "error", err.Error())
+		return nil, err
+	}
+	entry.logger.Info("finished reading parquet")
+
+	captions := []Captions{}
+	entry.logger.Info("unmarshalling data to struct")
+	if err := json.Unmarshal(data, &captions); err != nil {
+		logDebug.Error("unable to unmarshal data", "error", err.Error())
+		return nil, err
+	}
+	entry.logger.Info("finished unmarshalling data to struct")
+
+	return captions, err
+}
+
+func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	logDebug := logger.With("function", "main")
+	slog.SetDefault(logger)
+
+	var (
+		path              string = ""
+		addr              string = "127.0.0.1:8558"
+		remoteDatasetPath string = "https://github.com/mastrasec/vectoria/releases/download/demo_dataset/sbu_captions_embeddings.parquet"
+		embeddingLen      uint32 = 384
+	)
+
+	logger.Info("launching vector database")
 	entry, err := newEntrypoint(addr, path, true,
 		vectoria.WithIndexLSH(&vectoria.LSHConfig{
 			IndexName:      "demo",
 			NumRounds:      10,
 			NumHyperPlanes: 100,
-			SpaceDim:       1500,
+			SpaceDim:       embeddingLen,
 		}),
 	)
 	if err != nil {
-		log.Println(err)
+		logDebug.Error("unable to create entrypoint", "error", err.Error())
 		return
 	}
+	logger.Info("vector database is up")
+
+	data, err := entry.getData(remoteDatasetPath)
+	if err != nil {
+		logDebug.Error("unable to get data", "error", err.Error())
+		return
+	}
+	runtime.GC()
+
+	logger.Info("writing to database")
+	if err := entry.writeToDB(data); err != nil {
+		logDebug.Error("unable to write to database", "error", err.Error())
+		return
+	}
+	logger.Info("finished writing to database")
+	runtime.GC()
 
 	if err := entry.listen(); err != nil {
-		log.Println(err)
+		logDebug.Error("unable to listen to server", "error", err.Error())
 		return
 	}
 }
-
-type newReq struct {
-	Path     string                `json:"path"`
-	LSHConfs []*vectoria.LSHConfig `json:"lsh_configs"`
-}
-
-type newRes struct{}
